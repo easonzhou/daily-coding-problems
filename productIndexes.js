758b9dcb68b6e94baaec5be68a6a66870171f1de
Yin Sheng Zhou <yinsheng@sg.ibm.com>
2018-12-01 11:43:10 +0800

    add solution to problem 2

--- productIndexes.js
+++ productIndexes.js
@@ -8,15 +8,31 @@ If our input was [3, 2, 1], the expected output would be [2, 3, 6].
 Follow-up: what if you can't use division?
 */
 
-export default function productIndexes(input) {
+export function productIndexes(input) {
     let result = new Array(input.length).fill(0);
     let prod = 1;
 
     input.forEach(e => {
         prod = prod * e;
     });
-    
+
     result = result.map((e, i) => prod / input[i]);
+
+    return result;
+}
+
+export function productIndexesWithoutDivision(input) {
+    let result = new Array(input.length).fill(1);
+    let temp = 1;
+    
+    for(let i = 1; i < input.length; i++) {
+        result[i] = result[i - 1] * input[i - 1];
+    }
+
+    for(let i = input.length - 2; i >= 0; i--) {
+        temp = temp * input[i + 1];
+        result[i] = result[i] * temp; 
+    }
 
     return result;
 }
