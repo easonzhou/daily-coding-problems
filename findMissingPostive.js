993bce22b18038ca679222371a8bfa08be6c96d3
Yin Sheng Zhou <yinsheng@sg.ibm.com>
2018-12-04 00:52:13 +0800

    add question 3

---
+++ findMissingPostive.js
@@ -0,0 +1,37 @@
+/*
+Given an array of integers, find the first missing positive integer in linear time and constant space. 
+
+In other words, find the lowest positive integer that does not exist in the array. 
+
+The array can contain duplicates and negative numbers as well.
+
+For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+
+You can modify the input array in-place.
+*/
+
+export default function findMissingPositive(nums) {
+    let shift = 0, temp;
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] <= 0) {
+            temp = nums[shift];
+            nums[shift] = nums[i];
+            nums[i] = temp;
+            shift++;
+        }
+    }
+
+    for(let i = shift; i < nums.length; i++) {
+        if((Math.abs(nums[i]) - 1 >= 0) && (shift + Math.abs(nums[i]) - 1 < nums.length - 1)) {
+            nums[Math.abs(nums[i]) - 1 + shift] = -1 * nums[Math.abs(nums[i]) - 1 + shift];
+        }
+    }
+
+    for(let i = shift; i < nums.length; i++) {
+        if(nums[i] > 0) {
+            return i + 1 - shift;
+        }
+    }
+
+    return nums.length - shift + 1;
+}
--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 import twoItemsSum from './twoItemsSum';
 import { productIndexes, productIndexesWithoutDivision } from './productIndexes';
-console.log(twoItemsSum([10, 15, 3, 7], 17));
-console.log(twoItemsSum([1, 2, 4, 6, 9, 10, 21], 17));
-console.log(twoItemsSum([-2, 2, 4, 6, 9, 10, 21], 4));
+import findMissingPositive from './findMissingPostive';
+// console.log(twoItemsSum([10, 15, 3, 7], 17));
+// console.log(twoItemsSum([1, 2, 4, 6, 9, 10, 21], 17));
+// console.log(twoItemsSum([-2, 2, 4, 6, 9, 10, 21], 4));
 
-console.log(productIndexes([1, 2, 3, 4, 5]));
-console.log(productIndexes([3, 2, 1]));
+// console.log(productIndexes([1, 2, 3, 4, 5]));
+// console.log(productIndexes([3, 2, 1]));
 
-console.log(productIndexesWithoutDivision([1, 2, 3, 4, 5]));
-console.log(productIndexesWithoutDivision([3, 2, 1]));
+// console.log(productIndexesWithoutDivision([1, 2, 3, 4, 5]));
+// console.log(productIndexesWithoutDivision([3, 2, 1]));
+
+console.log(findMissingPositive([3, 4, -1, 1]));
+console.log(findMissingPositive([1, 2, 0]));
